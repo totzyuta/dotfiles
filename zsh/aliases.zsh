37769033a944234ee0db4ceb6f8ce70798bef4e2
totzYuta <yuta.totz@gmail.com>
2015-06-22 00:17:26 +0900

    Change /Users/totz/.go

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,13 +9,7 @@ alias chrome="open -a 'google chrome'"
 alias vi=/usr/local/bin/vim # its for brew vim
 
 # git
-alias "git log --graph"="git log --graph --branches --pretty=format:'%d [%h] \"%s\""
+alias gitlog="git log --graph --branches --pretty=format:'%d [%h] \"%s\"'"
 
 # bundler
 alias be="bundle exec"
-
-# go
-export GOROOT=/usr/local/Cellar/go/1.3.3/libexec
-export GOPATH=~/.go
-export GO_VERSION=1.3.3
-export PATH=$GOPATH/bin:$PATH
--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -7,7 +7,6 @@ export PATH=$PATH:$HOME/.rbenv/bin
 export PATH=$PATH:$HOME/bin
 
 # go
-export GOROOT=/usr/local/Cellar/go/1.3.3/libexec
-export GOPATH=~/.go
-export GO_VERSION=1.3.3
-export PATH=$PATH:$GOPATH/bin
+export GOROOT=/usr/local/opt/go/libexec
+export GOPATH=$HOME/.go
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
