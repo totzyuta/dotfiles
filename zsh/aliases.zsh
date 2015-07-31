670f3152b1b0d78b7f389798335950b2000a40ca
totzYuta <yuta.totz@gmail.com>
2015-07-31 10:03:24 +0900

    Add WiFi toggle switch

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,11 +1,16 @@
 # aliases
 
+# System
+# toggle WiFi network on/off
+# to enter network, `SSID PASS` follows
+alias ns="networksetup -setairportpower en0"
+# show your network interface name
+alias nc="/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport scan"
+
 # applications
-alias prv="open -a Preview"
-alias starttoday="open -a 'google chrome' | open -a mail | open -a ical | open -a pomodoro\ timer"
-alias dic="sdcv"
 alias o="open -a"
 alias chrome="open -a 'google chrome'"
+alias starttoday="open -a 'google chrome' | open -a mail | open -a ical | open -a iterm"
 alias vi=/usr/local/bin/vim # its for brew vim
 
 # bundler
