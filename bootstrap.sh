04354f4751516966ead8971aa4614bebd4f93e6b
totzYuta <yuta.totz@gmail.com>
2015-07-31 17:12:32 +0900

    :tophat: Take your hats off to the past, but take your coats off to the future.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,14 @@
 echo ">>> Linking dotfiles with home directory..."
+# Take your hats off to the past, but take your coats off to the future.
+# mv ~/.vimrc ~/.vimrc.org
+# mv ~/.xvimrc ~/.xvimrc.org
+# mv ~/.zshrc ~/.zshrc.org
+# mv ~/.tmux.conf ~/.tmux.conf.org
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-mv ~/.zshrc ~/.zshrc.org
+# use same vimrc for xcode
+ln -sf ~/dotfiles/.vimrc ~/.xvimrc
 ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.xvimrc ~/.xvimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 # Git Aliases
