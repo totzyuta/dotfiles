2ed77d538d383a89ec7806a000f4002a7556ffb2
totzyuta <yuta.totz@gmail.com>
2016-01-12 19:33:20 +0900

    :beer: Add some libraries for installing by homebrew

--- brew.sh
+++ brew.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+# Install Homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 # Make sure we’re using the latest Homebrew
 brew update
 
 # Upgrade any already-installed formulae
 brew upgrade
+
+# vim by homebrew
+brew install vim
+
+# rbenv
+brew install rbenv ruby-build
+
+# tmux
+brew install tmux reattach-to-user-namespace
 
 # easy jump tool
 # source setting script file in .zshrc
